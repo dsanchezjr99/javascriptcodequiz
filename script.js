7a3b92601970aaee7a887ca697189f684588ba64
Miguel David Sanchez <dsanchezjr99@gmail.com>
2021-06-17 22:03:16 -0400

    javascript

--- script.js
+++ script.js
@@ -65,4 +65,84 @@ function startQuiz() {
 function getQuestion() {
 
     letCurrentQuestion = questions[questionIndex];
+
+    let titleEl = document.getElementById("questions-title");
+    titleEl.textContent = currentQuestion.title;
+
+    choicesEl.innerHTML = "";
+
+    currentQuestion.choices.forEach(function(choice, i) {
+        let choiceSelect = document.createElement("button");
+        choiceSelect.setAttribute("class", "choice");
+        choiceSelect.setAttribute("value", choice);
+        choiceSelect.textContent = i + 1 + "." + choice;
+        choiceSelect.onclick = questionClickHandler;
+        choicesEl.appendChild(choiceSelect);
+
+    });
+}
+
+function questionCLick(){
+    if (this.value !== questions[questionIndex].answer){
+        time -=10;
+        if (time < 0) {
+            time = 0;
+        }
+        timerEl.textContent = time
+    }
+    questionIndex++;
+    if (questionIndex === questions.length){
+        quizEnd();
+    } else{
+        getQuestion();
+    }
+}
+
+function quizEnd() {
+    clearInterval(timerId);
+    let endScreenEl = document.getElementById("end-screen");
+    endScreenEl.removeAttribute("class");
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute("class", "hidden")
+}
+
+let finalScoreEl = document.getElementById("final-score");
+function clockTick(){
+    time--;
+    timerEl.textContent = time;
+    if(time <= 0) {
+        quizEnd();
+    }
+}
+
+submitButtonEL.onclick = () => {
+    let input = intialsEL.value;
+    console.log(time);
+    console.log(input);
+
+    let leaderBoard = [];
+    let topScores = JSON.parse(localStorage.getItem("score"));
+    if(topScores){
+        leaderBoard.push(...topScores);
+    }
+
+    leaderBoard.push({"user":input,"score":time});
+    console.log(leaderBoard);
+    //getScore
+
+    let table = ()=> {
+        for(let i = 0; i < leaderBoard.length; i++){
+            leaderBoard += names [i] + "," + scores[i];
+            let list = document.createElement("li");
+            list.setAttribute("value", highScores[i]);
+            highScores.appendChild("high-score");
+        }
+    }
+    localStorage.setItem("score", JSON.stringify(leaderBoard));
+    console.log("Hello")
+
+};
+
+startButtonEl.onclick = ()=> {
+    startQuiz();
 }
